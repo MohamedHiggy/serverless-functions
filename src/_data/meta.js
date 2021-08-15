352859a6cd1fbaafda1dfedae3ec50af2a187e57
hagooog <mohamed.higgy31@gmail.com>
2021-08-15 18:59:10 +0200

    first commit

---
+++ src/_data/meta.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // keep it short! shown in the header
+  title: 'Serverless fun',
+
+  // these are all optional and add links to the footer
+  repo: 'MohamedHiggy/serverless-functions',
+};
